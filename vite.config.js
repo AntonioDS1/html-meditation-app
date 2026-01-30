9cb9352b6f97154ac5b7f8fc0fc8b955628879a1
AntonioDS1 <159949547+AntonioDS1@users.noreply.github.com>
2026-01-30 19:14:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
-  base: mode === "production"
-    ? "/html-meditation-app/"
-    : "/",
-}));
+})
 
